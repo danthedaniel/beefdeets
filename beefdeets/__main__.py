94ccbb2ca56a8acbe72bef181f0e43d543b1b3cf
teaearlgraycold <supacoofoo@gmail.com>
2018-06-16 23:10:05 -0400

    Added arguments to the CLI.

--- beefdeets/__main__.py
+++ beefdeets/__main__.py
@@ -1,16 +1,30 @@
+#!/usr/bin/env python3
+
 import sys
+import argparse
 import os.path
 
 from .app import app
 
 
-def main():
+def start_app(host: str, port: int) -> None:
     deadbeef = app.config["player"].path
 
     if os.path.isfile(deadbeef):
-        app.run(host="0.0.0.0", port=8080)
+        app.run(host=host, port=port)
     else:
         print(f"DeaDBeeF executable {deadbeef} does not exist", file=sys.stderr)
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(prog="beefdeets",
+        description="Web UI to control DeaDBeeF.")
+    parser.add_argument("host", nargs='?', action="store", type=str,
+        default="0.0.0.0", help="Address to bind to.")
+    parser.add_argument("port", nargs='?', action="store", type=int,
+        default=8080, help="Port to bind to.")
+    args = parser.parse_args()
+    start_app(args.host, args.port)
 
 
 if __name__ == "__main__":
